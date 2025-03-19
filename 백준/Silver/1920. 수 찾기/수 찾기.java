272418ccec0d7f8e0076ebed6404d37b646e92d5
예진 <i0209i80@gmail.com>
2025-03-19 11:23:31 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 53420 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,30 +4,30 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder(); // 결과 출력을 위한 StringBuilder
+        StringBuilder sb = new StringBuilder();
 
-        // 1. N 입력받기
+        // N 입력받기
         int N = Integer.parseInt(br.readLine());
 
-        // 2. HashSet 선언 및 N개의 정수 입력받아 저장
-        Set<Integer> set1 = new HashSet<>();
+        // N개의 정수 입력받아 저장
+        Set<Integer> set1 = new HashSet<>(); // 수정: Set<Integer> (대문자 S)
         String[] inputNumbers1 = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
             set1.add(Integer.parseInt(inputNumbers1[i]));
         }
 
-        // 3. M 입력받기
+        // M 입력받기
         int M = Integer.parseInt(br.readLine());
 
-        // 4. List 선언 및 M개의 정수 입력받아 저장
-        List<Integer> list = new ArrayList<>();
+        // M개의 정수 입력받아 저장
+        List<Integer> list = new ArrayList<>(); // 수정: ArrayList<> (대문자 A)
         String[] inputNumbers2 = br.readLine().split(" ");
         for (int i = 0; i < M; i++) {
             list.add(Integer.parseInt(inputNumbers2[i]));
         }
 
-        // 5. M개의 수가 set1에 존재하는지 확인
-        for (int i = 0; i < list.size(); i++) {
+        // M개의 수가 set1에 존재하는지 확인
+        for (int i = 0; i < list.size(); i++) { // 수정: i < list.size(); (공백 추가)
             if (set1.contains(list.get(i))) {
                 sb.append("1\n");
             } else {
@@ -35,7 +35,6 @@ public class Main {
             }
         }
 
-        // 결과 출력
         System.out.print(sb.toString());
     }
 }
