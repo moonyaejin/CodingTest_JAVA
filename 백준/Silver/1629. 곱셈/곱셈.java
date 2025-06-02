a71cf913002b0a7099829ad094be7ec84dad35f1
예진 <i0209i80@gmail.com>
2025-06-02 12:31:57 +0900

    [Silver I] Title: 곱셈, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static long pow(long a, long b, long c) {
+        if (b == 0) return 1;
+        long half = pow(a, b / 2, c);
+        long result = (half * half) % c;
+
+        // 지수가 홀수라면 half**2 * a가 필요
+        if (b % 2 == 1) {
+            result = (result * a) % c;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+
+        System.out.println(pow(A, B, C));
+    }
+}
