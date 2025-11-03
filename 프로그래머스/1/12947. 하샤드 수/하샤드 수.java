7a8990a2a71cdaeff5e76a08e46988cc09aba4ad
문예진 <i0209i80@gmail.com>
2025-11-03 13:48:14 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(int x) {
+        String str = Integer.toString(x);
+        int sum = 0;
+        
+        for (int i = 0; i < str.length(); i++) {
+            sum += str.charAt(i) - '0';
+        }
+        
+        return x % sum == 0;
+    }
+}
