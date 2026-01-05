f95ecdf6212dd4e5e9debc7304e6c8400f11d19d
문예진 <i0209i80@gmail.com>
2026-01-05 16:27:03 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int total = brown + yellow;
+        
+        for (int height = 3; height <= (int)Math.sqrt(total); height++) {
+            if (total % height == 0) {
+                int width = total / height;
+                if (width < height) continue;
+                
+                if ((width - 2) * (height - 2) == yellow) { 
+                    return new int[] {width, height}; 
+                }
+            }
+        }
+        return new int[] {0, 0};
+    }
+}
